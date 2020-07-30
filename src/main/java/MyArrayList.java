9bd774b7bdbd56d4513f219b43bc18bed0e17067
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-30 13:18:39 -0500

    made more progress on the myArrayList

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,20 +1,26 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class MyArrayList <E extends Object>{
+public class MyArrayList <E>{
     private Object[] expansiveArray;
+    private int capacity;
+    private int size;
 
     public MyArrayList() {
         expansiveArray = new Object[0];
+        capacity = 0;
+        size = 0;
     }
 
     public MyArrayList(int size) {
         expansiveArray = new Object[size];
+        capacity = 0;
+        this.size = size;
     }
 
     @SuppressWarnings("unchecked")
-    private void populateArray(Object[] array, Object elementToSet) {
-        Arrays.fill(array, (E) elementToSet);
+    public void populateArray(Object elementToSet) {
+        Arrays.fill(expansiveArray, (E) elementToSet);
     }
 
     public int size() {
@@ -32,8 +38,4 @@ public class MyArrayList <E extends Object>{
         expansiveArray[index] = element;
         return temp;
     }
-
-//    public void add(E element) {
-//
-//    }
 }
--- src/test/java/MyArrayListTest.java
+++ src/test/java/MyArrayListTest.java
@@ -3,20 +3,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MyArrayListTest{
-    MyArrayList<String> strArrListNoLength;
-    MyArrayList<Integer> strArrListWithLength;
+    MyArrayList<String> intArrListNoLength;
+    MyArrayList<Integer> intArrListWithLength;
 
     @Before
     public void setup() {
-        strArrListNoLength = new MyArrayList<>();
-        strArrListWithLength = new MyArrayList<>(5);
+        intArrListNoLength = new MyArrayList<>();
+        intArrListWithLength = new MyArrayList<>(5);
+        intArrListWithLength.populateArray(0);
     }
 
     @Test
     public void myArrayListOneArgSizeTest() {
         int expectedSize = 0;
 
-        int actualSize = strArrListNoLength.size();
+        int actualSize = intArrListNoLength.size();
 
         assertEquals(expectedSize, actualSize);
     }
@@ -25,26 +26,26 @@ public class MyArrayListTest{
     public void myArrayListTwoArgSizeTest() {
         int expectedSize = 5;
 
-        int actualSize = strArrListWithLength.size();
+        int actualSize = intArrListWithLength.size();
 
         assertEquals(expectedSize, actualSize);
     }
 
-//    @Test
-//    public void setTest() {
-//        int expectedReturnValue = 0;
-//
-//        intArrList.set(2, 46);
-//        int actualSize = intArrList.;
-//
-//        assertEquals(expectedSize, actualSize);
-//    }
+    @Test
+    public void setTest() {
+        int expectedReturnValue = 46;
+
+        intArrListWithLength.set(2, 46);
+        int actualReturnValue = intArrListWithLength.get(2);
+
+        assertEquals(expectedReturnValue, actualReturnValue);
+    }
 
     @Test
     public void getTest() {
         int expectedReturnValue = 0;
 
-        int actualReturnValue = strArrListWithLength.get(2);
+        int actualReturnValue = intArrListWithLength.get(2);
 
         assertEquals(expectedReturnValue, actualReturnValue);
     }
